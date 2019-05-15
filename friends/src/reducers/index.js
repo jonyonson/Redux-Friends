ee15df2fb1e2f4edb9e454fca07478ba9f2d70a7
Jonathan Taylor <jonyonson@icloud.com>
2019-05-15 15:45:44 -0500

    initial boilerplate setup

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  deletingFriend: false,
+  fetchingFriends: false,
+  friends: [],
+  loggingIn: false,
+  savingFriends: false,
+  updatingFriend: false,
+  error: null,
+};

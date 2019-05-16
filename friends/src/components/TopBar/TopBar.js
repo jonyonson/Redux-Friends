16e9cf44dda48a49ad72da212e1552bf239483dc
Jonathan Taylor <jonyonson@icloud.com>
2019-05-16 16:38:03 -0500

    topbar component

---
+++ friends/src/components/TopBar/TopBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './TopBar.css';
+import { FiUserPlus, FiLogOut } from 'react-icons/fi';
+
+class TopBar extends Component {
+  logout = () => {
+    localStorage.setItem('token', '');
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div className="TopBar">
+        <div className="TopBar__inner">
+          <div className="TopBar__add-contact" onClick={this.props.addContact}>
+            <div>Add Friend</div>
+            <FiUserPlus color="#fafffd" size="24" />
+          </div>
+
+          <div className="TopBar__logout" onClick={this.logout}>
+            <div>Logout</div>
+            <FiLogOut color="#fafffd" size="24" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TopBar;
